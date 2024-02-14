7b30823fc0e855834588c460266552dc4a147157
david <fans@bxscience.edu>
2024-02-14 13:56:53 -0500

    changed img src on us/html to relative and back button href

--- src/app/flowers/page.tsx
+++ src/app/flowers/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return(
         <main>
             <div className="bg-red-500 flex gap-6">
-                <a href='/'>
+                <a href=''>
                 <button type="button" className="w-full flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100">
                     <svg className="w-5 h-5 rtl:rotate-180" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
